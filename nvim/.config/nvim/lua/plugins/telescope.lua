363875bdc691faa7e07fce245b78c8f502f5db2f
Abhiroop <abhiroopdatta7@gmail.com>
2025-03-15 21:09:47 +0000

    update telescope

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,12 +1,12 @@
 return {
     'nvim-telescope/telescope.nvim', tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    dependencies = { 'nvim-lua/plenary.nvim', 'nvim-tree/nvim-web-devicons' },
     keys = {
         -- Open File
-        { '<C-o>', '<cmd>Telescope find_files<cr>', desc = 'Open File' },
-        { '<leader>fo', '<cmd>Telescope find_files<cr>', desc = 'Open File' },
+        { '<C-o>', '<cmd>Telescope find_files find_command=rg,--ignore,--hidden,--files prompt_prefix=🔍<cr>', desc = 'Open File' },
+        { '<leader>fo', '<cmd>Telescope find_files find_command=rg,--ignore,--hidden,--files prompt_prefix=🔍<cr>', desc = 'Open File' },
         -- Search in Files
-        { '<C-S>', '<cmd>Telescope live_grep<cr>', desc = 'Search all files' },
+        { '<C-f>', '<cmd>Telescope live_grep<cr>', desc = 'Search all files' },
         { '<leader>ff', '<cmd>Telescope live_grep<cr>', desc = 'Search all files' },
         -- List files
         { '<leader>fb', '<cmd>Telescope buffers<cr>', desc = 'List Open files' },
