ceb0b6dec141cd94be5dab8f90d9041b64dec9bf
Abhiroop <abhiroopdatta7@gmail.com>
2025-03-16 18:49:39 +0000

    adding some more plugins

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,6 +1,15 @@
 return {
     'nvim-telescope/telescope.nvim', tag = '0.1.8',
     dependencies = { 'nvim-lua/plenary.nvim', 'nvim-tree/nvim-web-devicons' },
+    config = function()
+        require('telescope').setup {
+            pickers = {
+                find_files = {
+                    theme = "dropdown"
+                }
+            },
+        }
+    end,
     keys = {
         -- Open File
         { '<C-o>', '<cmd>Telescope find_files find_command=rg,--ignore,--hidden,--files prompt_prefix=🔍<cr>', desc = 'Open File' },
@@ -10,7 +19,9 @@ return {
         { '<leader>ff', '<cmd>Telescope live_grep<cr>', desc = 'Search all files' },
         -- List files
         { '<leader>fb', '<cmd>Telescope buffers<cr>', desc = 'List Open files' },
-
+        -- coomand
+        { '<leader>fc', '<cmd>builtin.commands<cr>', desc = 'Builtin commands' },
+        
         -- Not sure what it does
         { '<leader>ft', '<cmd>Telescope help_tags<cr>', desc = 'Telescope help_tags' },
     },
